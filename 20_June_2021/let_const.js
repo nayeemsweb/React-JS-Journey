fe33ca2e3dd6d8f88224393c6c74dab547a40b8a
Nayeem Rahman <nayeem12rahman@gmail.com>
2021-06-20 00:42:51 +0600

    Trying Let & Const keywords

---
+++ 20_June_2021/let_const.js
@@ -0,0 +1,20 @@
+//var
+var myName = 'Nayeem';
+console.log(myName);
+
+myName = 'Sabbir';
+console.log(myName);
+
+//let
+let yourName = ('Something');
+console.log(yourName);
+
+yourName = 'Anything';
+console.log(yourName);
+
+//const
+const hisName = 'get';
+console.log(hisName);
+
+hisName = 'set';
+console.log(hisName);
