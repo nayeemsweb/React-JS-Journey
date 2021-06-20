dbdce66c2b00f4f983c059776c1b30cd8fe787a8
Nayeem Rahman <nayeem12rahman@gmail.com>
2021-06-20 11:49:10 +0600

    Export & Import Module

---
+++ 20_June_2021/Exports_imports.js
@@ -0,0 +1,11 @@
+//default export
+import person from "./person"
+import prs from "./person"
+
+import { baseData } from "./utility"
+import { clean } from "./utility"
+
+//named export 
+import {smth} from './utility'
+import {smth as Smth} from './utility'
+import * as bundled from './utility'
