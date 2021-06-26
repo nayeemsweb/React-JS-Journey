da093b358670bf3eea48520a55798015f61a43f2
Nayeem Rahman <nayeem12rahman@gmail.com>
2021-06-27 01:04:37 +0600

    Destructuring

---
+++ 27_June_2021/Destructure.js
@@ -0,0 +1,12 @@
+// Array Destructuring
+[a,b] = ['Hello', 'Nayeem'];
+console.log(a);
+console.log(b);
+
+const numbers = [1, 2, 3];
+[num1, num2] = numbers; // 1,2
+[num1, , num3] = numbers; // 1, 3
+console.log(num1, num2); 
+console.log(num1, num3); 
+
+
