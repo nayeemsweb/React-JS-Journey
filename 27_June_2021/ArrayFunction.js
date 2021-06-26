b0ba4de13732cbb019dd57717ac1a4e3938ae450
Nayeem Rahman <nayeem12rahman@gmail.com>
2021-06-27 01:17:32 +0600

    Array Function

---
+++ 27_June_2021/ArrayFunction.js
@@ -0,0 +1,15 @@
+const numbers = [1, 2, 3];
+const doubledNumArray = numbers.map((num) => {
+    return num * 2;
+});
+
+console.log(numbers);
+console.log(doubledNumArray);
+
+const anotherNumber = [2, 4, 6];
+const doubledAnotherNumber = anotherNumber.map((anotherNum) => {
+    return anotherNum * 3;
+});
+
+console.log(anotherNumber);
+console.log(doubledAnotherNumber);
